6608acefeedc6a55f954d6e9b04e8bfc05f6c15a
camkweston <115127563+camkweston@users.noreply.github.com>
2026-04-26 16:37:41 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,26 @@
+from functools import lru_cache
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        N = len(nums)
+        
+        if N == 0:
+            return 0
+        
+        if N == 1:
+            return nums[0]
+        
+        dp = [0] * N
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, N):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        
+        return dp[N - 1]
+
+
+
+        
